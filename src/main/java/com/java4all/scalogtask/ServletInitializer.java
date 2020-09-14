39ffe243b701758ffb694b58602888768470ff03
汪忠祥 <1186355422@qq.com>
2020-09-14 14:17:46 +0800

    init

---
+++ src/main/java/com/java4all/scalogtask/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.java4all.scalogtask;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ScalogTaskApplication.class);
+    }
+
+}
